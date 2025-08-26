03c7a83a3f8f18830af655f13f793d464caa8320
M'hamed El Alami <elalamimhamed00@gmail.com>
2025-08-26 18:54:46 +0100

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,7 @@ from django.views.generic import RedirectView
 from users.views import RegisterView, UserProfileView, CustomLoginView, home
 from activities.views import ActivityListCreateView, ActivityDetailView, ActivitySummaryView
 from rest_framework_simplejwt.views import TokenRefreshView
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,7 +16,8 @@ urlpatterns = [
     path('api/', RedirectView.as_view(url='/', permanent=False)),
 
     # Registration
-    path('api/auth/register/', RegisterView.as_view(), name='register'),
+    # path('api/auth/register/', RegisterView.as_view(), name='register'),
+    path('api/auth/register/', csrf_exempt(RegisterView.as_view()), name='register'),
 
     # User profile
     path('api/auth/profile/', UserProfileView.as_view(), name='user-profile'),
@@ -25,7 +27,8 @@ urlpatterns = [
     path('api/activities/<int:pk>/', ActivityDetailView.as_view(), name='activity_detail'),
 
     # JWT token endpoints
-    path('api/auth/login/', CustomLoginView.as_view(), name='login'),
+    # path('api/auth/login/', CustomLoginView.as_view(), name='login'),
+    path('api/auth/login/', csrf_exempt(CustomLoginView.as_view()), name='login'),
     path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # Activity summary
