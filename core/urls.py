5f5c50e9e0d572ae58ba9a7facc6b244de9feba1
M'hamed El Alami <elalamimhamed00@gmail.com>
2025-08-26 22:00:51 +0100

    Final code cleanup

--- core/urls.py
+++ core/urls.py
@@ -1,39 +1,3 @@
-# from django.urls import path, re_path
-# from django.contrib import admin
-# from django.views.generic import RedirectView
-# from users.views import RegisterView, UserProfileView, CustomLoginView, home
-# from activities.views import ActivityListCreateView, ActivityDetailView, ActivitySummaryView
-# from rest_framework_simplejwt.views import TokenRefreshView
-
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-    
-#     # Landing page
-#     path('', home, name='home'), 
-    
-#     # Optional: redirect /api/ to landing page
-#     path('api/', RedirectView.as_view(url='/', permanent=False)),
-
-#     # Registration
-#     path('api/auth/register/', RegisterView.as_view(), name='register'),
-
-#     # User profile
-#     path('api/auth/profile/', UserProfileView.as_view(), name='user-profile'),
-
-#     # Activities
-#     path('api/activities/', ActivityListCreateView.as_view(), name='activities_list_create'),
-#     path('api/activities/<int:pk>/', ActivityDetailView.as_view(), name='activity_detail'),
-
-#     # JWT token endpoints
-#     path('api/auth/login/', CustomLoginView.as_view(), name='login'),
-#     path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-#     # Activity summary
-#     path('api/activities/summary/', ActivitySummaryView.as_view(), name='activities_summary'),
-# ]
-
-
 from django.urls import path
 from django.contrib import admin
 from users.views import RegisterView, UserProfileView, CustomLoginView, home
